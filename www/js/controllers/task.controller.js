c8a071105c29e997d7a3168d16ef4319b0336150
Heitor Zimmer da Silva <hsilva@automasafety.com.br>
2016-04-20 12:37:45 -0300

    Edicao de tarefas

--- www/js/controllers/task.controller.js
+++ www/js/controllers/task.controller.js
@@ -1,53 +1,65 @@
-controllers.controller('TaskCtrl',function($scope, TaskService, $ionicPopup){
-    $scope.lista = TaskService.recuperarTarefas();
+controllers.controller('TaskCtrl', function($scope, TaskService, $ionicPopup, $ionicListDelegate){
+  $scope.lista = TaskService.recuperarTarefas();
 
-    $scope.showPopup = function() {
+  $scope.showPopup = function(item) {
+    var isUpdate = angular.isDefined(item);
+    if(isUpdate){
+      $scope.task = item
+    }else{
       $scope.task = {
-        pronto: false
-      };
+      pronto: false
+    };
+  }
 
-      // An elaborate, custom popup
-      var myPopup = $ionicPopup.show({
-        template: '<input type="text" ng-model="task.nome">',
-        title: 'Escreva a tarefa',
-        subTitle: 'Seja suscinto e objetivo',
-        scope: $scope,
-        buttons: [
-          { text: 'Cancelar' },
-          {
-            text: '<b>Salvar</b>',
-            type: 'button-positive',
-            onTap: function(e) {
-              if (!$scope.task.nome) {
-                alert('descricao obrigatoria.');
-                e.preventDefault();
-              } else {
+  // An elaborate, custom popup
+  var myPopup = $ionicPopup.show({
+      template: '<input type="text" ng-model="task.nome">',
+      title: 'Escreva a tarefa',
+      subTitle: 'Seja suscinto e objetivo',
+      scope: $scope,
+      buttons: [
+        { text: 'Cancelar' },
+        {
+          text: '<b>Salvar</b>',
+          type: 'button-positive',
+          onTap: function(e) {
+            if (!$scope.task.nome) {
+              alert ('Nome obrigatorio');
+              e.preventDefault();
+            } else {
+              if(!isUpdate){
                 $scope.lista.push($scope.task);
               }
+              $ionicListDelegate.closeOptionButtons();
             }
           }
-        ]
-      });
+        }
+      ]
+    });
   };
-  $scope.toggleDelete = function(){
-    $scope.shouldShowDelete = !$scope.shouldShowDelete;
-  };
-  $scope.deletar = function(item){
-    $scope.lista.splice($scope.lista.indexOf(item), 1);
-  };
-  $scope.showConfirm = function(item) {
-     var confirmPopup = $ionicPopup.confirm({
-       title: 'Excluir Tarefa',
-       template: 'Voce tem certeza que quer excluir essa tarefa?'
-     });
 
-     confirmPopup.then(function(res) {
-       if(res) {
-         $scope.deletar(item);
-         alert('Tarefa excluida com sucesso')
-       } else {
-         console.log('You are not sure');
-       }
-     });
-   };
+$scope.toggleDelete = function() {
+  $scope.shouldShowDelete = !$scope.shouldShowDelete;
+}
+
+$scope.deletar = function(item) {
+  $scope.lista.splice($scope.lista.indexOf(item), 1);
+}
+
+$scope.showConfirm = function(item) {
+  var confirmPopup = $ionicPopup.confirm({
+    title: 'Excluir tarefa',
+    template: 'Voce tem certeza que quer excluir esta tarefa?'
+  });
+
+confirmPopup.then(function(res) {
+    if(res) {
+      $scope.deletar(item);
+      alert ('Tarefa excluida com sucesso!')
+    } else {
+      console.log('You are not sure');
+    }
+  });
+};
+
 });
