c8ba23d9315653f27a056536653d846d43cff0c6
Heitor Zimmer da Silva <hsilva@automasafety.com.br>
2016-04-20 10:03:38 -0300

    separando os arquivos em modulos

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.configs', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,6 +22,11 @@ var app = angular.module('starter', ['ionic'])
     }
   });
 });
+
+var controllers = angular.module('starter.controllers', []);
+var services = angular.module('starter.services',[]);
+var configs = angular.module('starter.configs',[]);
+
 app.controller('mainCtrl',function($scope, TaskService){
     $scope.lista = TaskService.recuperarTarefas();
 });
